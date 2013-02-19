bb653e485c87e7c13430fea9af0e21624fde5b16
Kaynshin <kaynshin.web@gmail.com>
2013-02-19 17:41:32 +0100

    clean du dossier

--- html/includes/bottom.php
+++
@@ -1,3 +0,0 @@
-<footer class="bar-title bar-footer bar-tab">
-    <p class="title">&copy; C2iS</p>
-</footer>
--- html/includes/top.php
+++
@@ -1,4 +0,0 @@
-<!-- Make sure all your bars are the first things in your <body> -->
-<header class="bar-title">
-    <h1 class="title">Ratchet</h1>
-</header>
